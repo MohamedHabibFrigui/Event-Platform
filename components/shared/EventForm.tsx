71403c5086d2943e4705184cb4b1ba3142a0e1f6
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-12-16 19:17:20 +0100

    Add file uploader

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -17,6 +17,9 @@ import { Input } from "@/components/ui/input";
 import { eventFormSchema } from "@/lib/validator";
 import { eventDefaultValues } from "@/constants";
 import Dropdown from "./Dropdown";
+import { Textarea } from "@/components/ui/textarea";
+import { FileUploader } from "./FileUploader";
+import { useState } from "react";
 
 type EventformProps = {
   userId: string;
@@ -24,6 +27,8 @@ type EventformProps = {
 };
 
 const EventForm = ({ userId, type }: EventformProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+
   const initialValues = eventDefaultValues;
 
   const form = useForm<z.infer<typeof eventFormSchema>>({
@@ -74,6 +79,42 @@ const EventForm = ({ userId, type }: EventformProps) => {
             )}
           />
         </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="h-72">
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    className="textarea rounded-2xl"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="h-72">
+                  <FileUploader
+                    onFieldChange={field.onChange}
+                    imageUrl={field.value}
+                    setFiles={setFiles}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
